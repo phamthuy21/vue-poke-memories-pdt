7027636660e6766b2534470311f8457a1e7fd554
phamthuy21 <phamthuy2192001@gmail.com>
2022-06-27 01:56:46 +0700

    Complete game poke memories with vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <main-screen
+    v-if="statusMatch === 'default'"
+    @onStart="onHandelBeforeStart($event)"
+  />
+  <interact-screen
+    v-if="statusMatch === 'match'"
+    :cardsContext="settings.cardsContext"
+    @onFinish="onGetResult"
+  />
+  <result-screen
+    v-if="statusMatch === 'result'"
+    :timer="timer"
+    @onStartAgain="statusMatch = 'default'"
+  />
+  <copy-right-screen />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import MainScreen from "./components/MainScreen.vue";
+import CopyRightScreen from "./components/CopyRightScreen.vue";
+import InteractScreen from "./components/InteractScreen.vue";
+import { shuffled } from "./utils/array";
+import ResultScreen from "./components/ResultScreen.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    MainScreen,
+    InteractScreen,
+    ResultScreen,
+    CopyRightScreen,
+  },
+  data() {
+    return {
+      settings: {
+        totalOfBlock: 0,
+        cardsContext: [],
+        startedAt: null,
+      },
+      statusMatch: "default",
+      timer: 0,
+    };
+  },
+  methods: {
+    onHandelBeforeStart(config) {
+      console.log("running handle before start, ", config);
+      this.settings.totalOfBlock = config.totalOfBlock;
+
+      const firstCards = Array.from(
+        { length: this.settings.totalOfBlock / 2 },
+        (_, i) => i + 1
+      );
+
+      const secondCards = [...firstCards];
+      const cards = [...firstCards, ...secondCards];
+      this.settings.cardsContext = shuffled(
+        shuffled(shuffled(shuffled(cards)))
+      );
+
+      this.settings.startedAt = new Date().getTime();
+      //data ready
+      this.statusMatch = "match";
+    },
+    onGetResult() {
+      //get timmer
+      this.timer = new Date().getTime() - this.settings.startedAt;
+
+      //switch to result component
+      this.statusMatch = "result";
+    },
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/CopyRightScreen.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="copyright">
+    <p>
+      This game owned by PDT in Vue 3 -
+      <a href="#">view here</a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css" scoped>
+.copyright {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 1rem;
+  color: var(--light);
+  z-index: 3;
+  font-size: 1rem;
+}
+.copyright a {
+  color: #f4dc26;
+}
+</style>
